2cbb509db2abffdded5b7964cf72701064ab27e7
wkdewey@gmail.com <wkdewey@gmail.com>
2018-07-02 14:57:35 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,10 @@
 def turn(board)
-  display_board(board)
   puts "Please enter 1-9:"
   input = gets
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board,index)
+    display_board(board)
   else
     turn(board)
   end
